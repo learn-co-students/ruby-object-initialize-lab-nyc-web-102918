4b05d1a035d5d5376648a680fe5424b95b3ee71b
vibhu mahendru <vibhumahendru@vibhus-MacBook-Pro.local>
2018-10-30 15:13:05 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+
+  def initialize (name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  # def initialize(*args)
+  #   @name = args[0]
+  #   @breed = args[1] || "Mutt"
+  # end
+
+end
